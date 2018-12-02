c63e37cf9ac10b13af00976ff85091609a1638eb
N.L.N.Rao <nellutlalnrao@gmail.com>
2018-12-02 23:34:29 +0530

    Fixed few content in Front page

--- init_default.php
+++ init_default.php
@@ -41,6 +41,7 @@ function homeSlider() {
 }
 </script>
 <style>
+body { overflow-y:scroll; }
 #myCarousel .nav a small { display: block; }
 #myCarousel .nav { background: #eee; }
 .nav-justified > li > a { border-radius: 0px; }
@@ -52,7 +53,8 @@ function homeSlider() {
 
 <script type="text/javascript">
 $(document).ready(function(){
-//  selectAppInitHeader('appInitHeader-home');
+ selectAppInitHeader('appInitHeader-home');
+ $("#sliderCarousel").carousel({interval: 4000});
   homeSlider();
   if(getCookie("LiveSupportChat")===''){
    var chatData=[{"title":"AssignmentHelp","msg":"Hi, Do you need assignment help?"}];
@@ -69,28 +71,114 @@ a.a-black,a.a-black:hover { color:#000; }
 <div id="chat_div"></div>
 <?php include_once 'templates/api_init_header.php'; ?>
 
-<div> 
+ <!-- Modal ::: Start -->
+ <!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
 
-  <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width:100%;max-height:500px;">
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Add your Review</h4>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+		
+		  <div class="row">
+		    <div class="col-xs-12">
+			  <div class="form-group">
+			    <label>Title</label>
+				<input type="text" class="form-control" placeholder="Enter your Title"/>
+			  </div>
+			</div>
+		  </div>
+		  
+		  <div class="row">
+		    <div class="col-xs-12">
+			  <div class="form-group">
+			    <label>Your Feedback</label>
+				<textarea class="form-control" placeholder="Enter your Feedback"></textarea>
+			  </div>
+			</div>
+		  </div>
+		  
+		  <div class="row">
+		    <div align="right" class="col-xs-12">
+			  <i class="fa fa-star-o" aria-hidden="true"></i>
+			  <i class="fa fa-star-o" aria-hidden="true"></i>
+			  <i class="fa fa-star-o" aria-hidden="true"></i>
+			  <i class="fa fa-star-o" aria-hidden="true"></i>
+			  <i class="fa fa-star-o" aria-hidden="true"></i>
+			</div>
+		  </div>
+		  
+		  <div class="row">
+		    <div class="col-xs-12">
+			    <button class="btn btn-primary form-control" style="margin-top:10px;"><b>Add Review</b></button>
+			</div>
+		  </div>
+		  
+		</div>
+      </div>
+    </div>
+
+  </div>
+</div>
+ <!-- Modal ::: End -->
+
+<div> 
+<style>
+@import url(https://fonts.googleapis.com/css?family=Open+Sans:600);
+body { font-family:'Open Sans','sans-serif'; }
+.item-1,.item-2,.item-3{ position:absolute;display:block;top:2em;width:60%;font-size:2em;animation-duration: 20s;
+	animation-timing-function: ease-in-out;animation-iteration-count: infinite;color:#fff; }
+.item-1{ animation-name: anim-1; }
+.item-2{ animation-name: anim-2; }
+.item-3{ animation-name: anim-3; }
+@keyframes anim-1 {
+  0%, 8.3% { left: -100%; opacity: 0; }
+  8.3%,25% { left: 25%; opacity: 1; }
+  33.33%, 100% { left: 110%; opacity: 0; }
+}
+@keyframes anim-2 {
+  0%, 33.33% { left: -100%; opacity: 0; }
+  41.63%, 58.29% { left: 25%; opacity: 1; }
+  66.66%, 100% { left: 110%; opacity: 0; }
+}
+@keyframes anim-3 {
+  0%, 66.66% { left: -100%; opacity: 0; }
+  74.96%, 91.62% { left: 25%; opacity: 1; }
+  100% { left: 110%; opacity: 0; }
+}
+</style>
+  <div style="width:100%;position:absolute;z-index:1000;margin-top:6%;">
+  <p class="item-1">This is your last chance. After this, there is no turning back.</p>
+
+<p class="item-2">You take the blue pill - the story ends, you wake up in your bed and believe whatever you want to believe.</p>
+
+<p class="item-3">You take the red pill - you stay in Wonderland and I show you how deep the rabbit-hole goes.</p>
+  </div>
+  <div id="sliderCarousel" class="carousel slide" data-ride="carousel" style="width:100%;max-height:500px;">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#sliderCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#sliderCarousel" data-slide-to="1"></li>
+      <li data-target="#sliderCarousel" data-slide-to="2"></li>
     </ol>
 
     <!-- Wrapper for slides -->
     <div class="carousel-inner">
       <div class="item active">
-        <img src="https://www.w3schools.com/bootstrap/la.jpg" alt="Los Angeles" style="width:100%;">
+        <img src="images/1.png" alt="Los Angeles" style="width:100%;">
       </div>
 
       <div class="item">
-        <img src="https://www.w3schools.com/bootstrap/chicago.jpg" alt="Chicago" style="width:100%;">
+        <img src="images/2.png" alt="Chicago" style="width:100%;">
       </div>
     
       <div class="item">
-        <img src="https://www.w3schools.com/bootstrap/ny.jpg" alt="New york" style="width:100%;">
+        <img src="images/3.png" alt="New york" style="width:100%;">
       </div>
     </div>
 
@@ -221,6 +309,229 @@ a.a-black,a.a-black:hover { color:#000; }
 <div align="center" class="mtopbot100p">
 <h2 class="mbot20p"><b>Stylish Portfolio is the perfect theme for your next project!</b></h2>
 This theme features a flexible, UX friendly sidebar menu and stock photos from our friends at Unsplash!
+</div>
+
+<div align="center" style="background-color:#ccc;">
+<div class="container-fluid" style="padding-top:25px;padding-bottom:45px;">
+<div class="row">
+
+<div align="center" class="col-md-4">
+<h1><b>882096</b></h1>
+<h5><b>DELIVERED ORDERS</b></h5>
+</div>
+
+<div class="col-md-4">
+<h1><b>5000</b></h1>
+<h5><b>EXPERTS</b></h5>
+</div>
+
+<div class="col-md-4">
+<h1><b>4.9/5</b></h1>
+<h5><b>CLIENT RATING</b></h5>
+</div>
+
+</div>
+</div>
+</div>
+
+<div style="background-color:#337ab7;color:#fff;padding-top:20px;padding-bottom:20px;">
+<div class="container-fluid">
+
+<div class="row">
+<div align="center" class="col-xs-12">
+<h2><b>Student's Testimonials</b></h2>
+</div>
+<div align="center" class="col-xs-12" style="margin-top:10px;">
+Here are the top 6 Feedbacks from the Students who used hereweget.com for Assignment Help.
+</div>
+</div>
+
+<div class="row">
+<div align="center" class="col-xs-12 col-md-4" style="margin-top:20px;">
+
+<div class="list-group">
+
+<div class="list-group-item" style="color:#000;">
+<h4 align="center" style="color:#337ab7;"><b>GREAT WORK IS DONE</b></h4>
+Thank you so much for your help. This is good as I wanted. The Writer was 
+best and I would like to give all my assignment to him/her next time.
+</div>
+
+<div align="right" class="list-group-item" style="background-color:#ff9800;">
+
+<div><b>N.L.N.Rao, India</b></div>
+<div>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-o" aria-hidden="true"></i>
+</div>
+<div><b>posted on 22 August 2018</b></div>
+
+</div>
+
+</div>
+
+</div>
+
+<div align="center" class="col-xs-12 col-md-4" style="margin-top:20px;">
+
+<div class="list-group">
+
+<div class="list-group-item" style="color:#000;">
+<h4 align="center" style="color:#337ab7;"><b>GREAT WORK IS DONE</b></h4>
+Thank you so much for your help. This is good as I wanted. The Writer was 
+best and I would like to give all my assignment to him/her next time.
+</div>
+
+<div align="right" class="list-group-item" style="background-color:#ff9800;">
+
+<div><b>N.L.N.Rao, India</b></div>
+<div>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-o" aria-hidden="true"></i>
+</div>
+<div><b>posted on 22 August 2018</b></div>
+
+</div>
+
+</div>
+
+</div>
+
+<div align="center" class="col-xs-12 col-md-4" style="margin-top:20px;">
+
+<div class="list-group">
+
+<div class="list-group-item" style="color:#000;">
+<h4 align="center" style="color:#337ab7;"><b>GREAT WORK IS DONE</b></h4>
+Thank you so much for your help. This is good as I wanted. The Writer was 
+best and I would like to give all my assignment to him/her next time.
+</div>
+
+<div align="right" class="list-group-item" style="background-color:#ff9800;">
+
+<div><b>N.L.N.Rao, India</b></div>
+<div>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-o" aria-hidden="true"></i>
+</div>
+<div><b>posted on 22 August 2018</b></div>
+
+</div>
+
+</div>
+
+</div>
+
+<div align="center" class="col-xs-12 col-md-4" style="margin-top:20px;">
+
+<div class="list-group">
+
+<div class="list-group-item" style="color:#000;">
+<h4 align="center" style="color:#337ab7;"><b>GREAT WORK IS DONE</b></h4>
+Thank you so much for your help. This is good as I wanted. The Writer was 
+best and I would like to give all my assignment to him/her next time.
+</div>
+
+<div align="right" class="list-group-item" style="background-color:#ff9800;">
+
+<div><b>N.L.N.Rao, India</b></div>
+<div>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-o" aria-hidden="true"></i>
+</div>
+<div><b>posted on 22 August 2018</b></div>
+
+</div>
+
+</div>
+
+</div>
+
+<div align="center" class="col-xs-12 col-md-4" style="margin-top:20px;">
+
+<div class="list-group">
+
+<div class="list-group-item" style="color:#000;">
+<h4 align="center" style="color:#337ab7;"><b>GREAT WORK IS DONE</b></h4>
+Thank you so much for your help. This is good as I wanted. The Writer was 
+best and I would like to give all my assignment to him/her next time.
+</div>
+
+<div align="right" class="list-group-item" style="background-color:#ff9800;">
+
+<div><b>N.L.N.Rao, India</b></div>
+<div>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-o" aria-hidden="true"></i>
+</div>
+<div><b>posted on 22 August 2018</b></div>
+
+</div>
+
+</div>
+
+</div>
+
+<div align="center" class="col-xs-12 col-md-4" style="margin-top:20px;">
+
+<div class="list-group">
+
+<div class="list-group-item" style="color:#000;">
+<h4 align="center" style="color:#337ab7;"><b>GREAT WORK IS DONE</b></h4>
+Thank you so much for your help. This is good as I wanted. The Writer was 
+best and I would like to give all my assignment to him/her next time.
+</div>
+
+<div align="right" class="list-group-item" style="background-color:#ff9800;">
+
+<div><b>N.L.N.Rao, India</b></div>
+<div>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star" aria-hidden="true"></i>
+<i class="fa fa-star-o" aria-hidden="true"></i>
+</div>
+<div><b>posted on 22 August 2018</b></div>
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+<div class="row">
+<div align="center" class="col-xs-12 col-md-12" style="margin-top:20px;">
+ <div class="btn-group">
+  <button class="btn btn-default" style="color:#000;border:1px solid #000;"><b>View All Reviews</b></button>
+  <button class="btn btn-default" style="color:#000;border:1px solid #000;" 
+  data-toggle="modal" data-target="#myModal"><b>Add New Review</b></button>
+ </div>
+</div>
+</div>
+</div>
+</div>
+
+<div class="container-fluid">
+ <div align="center" class="col-xs-12">
+   <img src="images/anups-logo.jpg" style="width:250px;height:auto;margin-top:25px;margin-bottom:25px;"/>
+ </div>
 </div>
 
 </body>
--- templates/api_init_header.php
+++ templates/api_init_header.php
@@ -1,25 +1,30 @@
 <style>
 .navbar-nav>li>a:hover,.navbar-nav>li>a:focus { background-color:#fff; }
 .navbar-custom { border-bottom:2px solid #000; }
-.navbar-brand-span { font-family:logoTitle;font-size:32px;color:#000; }
-@font-face { font-family:logoTitle;src:url('fonts/LitchisIsland.ttf'); }
+.navbar-brand-span { font-family:logoTitle;font-size:28px;color:#000; }
+@font-face { font-family:logoTitle;src:url('fonts/Boogaloo-Regular.otf'); }
 @font-face { font-family:specialHeading01;src:url('fonts/Boogaloo-Regular.otf'); }
 .header-active { color:#000;padding:5px;border:2px solid #000; }
 .header-inactive { color:#000; }
 .a-white { color:#fff; }
 </style>
 <script type="text/javascript">
-function selectAppInitHeader(id){
+function selectAppInitHeader(id){ 
  var arry_Id=["appInitHeader-home","appInitHeader-aboutUs","appInitHeader-services","appInitHeader-howItWorks",
-              "appInitHeader-auth","appInitHeader-contactUs"];
+              "appInitHeader-pricequote","appInitHeader-auth","appInitHeader-contactUs"];
  for(var index=0;index<arry_Id.length;index++){
    if(arry_Id[index]===id){
       if(!$('#'+arry_Id[index]).hasClass('active')) { $('#'+arry_Id[index]).addClass('active'); }// header-active
 	  if($('#'+arry_Id[index]).hasClass('active')) { $('#'+arry_Id[index]).removeClass('active'); }
+	  $('#'+arry_Id[index]).css('background-color','#fff');
+	  $('#'+arry_Id[index]+">a").css('color','#000');
+	  
    }
    else { 
-      if($('#'+arry_Id[index]).hasClass('active')) { $('#'+arry_Id[index]).removeClass('active'); }
+      if($('#'+arry_Id[index]).hasClass('active')) { $('#'+arry_Id[index]).addClass('active'); }
 	  if(!$('#'+arry_Id[index]).hasClass('active')) { $('#'+arry_Id[index]).removeClass('active'); }
+	  $('#'+arry_Id[index]+">a").css('color','#fff');
+	  $('#'+arry_Id[index]).css('background-color','#000');
    }
  }
 }
@@ -52,16 +57,108 @@ $(document).ready(function(){
 });
 </script>
 <style>
-ul#al-menu>li>a { color:#fff; }
-ul#al-menu>li.active { background-color:#fff; }
-ul#al-menu>li.active>a { color:#000; }
-ul#al-menu>li:hover { background-color:#ccc; }
-ul#al-menu>li:hover>a { color:#000; }
+.navbar-nav>li>a:hover { background-color:#000;color:#fff; }
+.navbar-nav>li>a:active,.navbar-nav>li>a:focus { background-color:#fff;color:#000; }
 body { height:150px;overflow-y:scroll;border:1px solid #000; }
 body::-webkit-scrollbar-track { -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);background-color: #F5F5F5; }         
 body::-webkit-scrollbar { width: 6px;background-color: #F5F5F5; }         
 body::-webkit-scrollbar-thumb { background-color: #000000; }
 </style>
+<!-- Modal ::: Start -->
+<!-- Modal -->
+<div id="priceQuotationModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+  
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Price Quotation</h4>
+      </div>
+      <div class="modal-body">
+        <div class="container-fluid">
+		
+		  <!-- Email -->
+		  <div class="row">
+		    <div class="col-xs-12">
+			  <div class="form-group">
+			    <label>Email</label>
+				<input type="text" class="form-control" placeholder="Enter your Email"/>
+			  </div>
+			</div>
+		  </div>
+		  <!-- Email -->
+		  
+		  <!-- Subject -->
+		  <div class="row">
+		    <div class="col-xs-12">
+			  <div class="form-group">
+			    <label>Subject</label>
+				<input type="text" class="form-control" placeholder="Enter your Subject"/>
+			  </div>
+			</div>
+		  </div>
+		  <!-- Subject -->
+		  
+		  <!-- Number Pages -->
+		  <div class="row">
+		    <div class="col-xs-12 col-md-6">
+			 <div class="form-group">
+			   <label>Pages</label>
+		       <div class="input-group">
+				  <span class="input-group-addon">-</span>
+				  <input type="text" class="form-control" placeholder="Enter pages" value="1">
+				  <span class="input-group-addon">+</span>
+				  <span class="input-group-addon">250 Words</span>
+			   </div>
+		     </div>
+			</div>
+			
+			<div class="col-xs-12 col-md-6">
+			 <div class="form-group">
+			   <label>Country</label>
+			   <select class="form-control">
+			     <option value="">Select your Country</option>
+				 <option value="India">India</option>
+				 <option value="Australia">Australia</option>
+			   </select>
+			 </div>
+			</div>
+		  </div>
+		  <!-- Subject -->
+		  
+		  <!-- Number Pages -->
+		  <div class="row">
+		    <div class="col-xs-12 col-md-6">
+			 <div class="form-group">
+			   <label>Deadline Date</label>
+		       <input type="date" class="form-control"/>
+		     </div>
+			</div>
+			
+			<div class="col-xs-12 col-md-6">
+			 <div class="form-group">
+			   <label>Deadline Time</label>
+			   <input type="time" class="form-control"/>
+			 </div>
+			</div>
+		  </div>
+		  <!-- Subject -->
+		  
+		  <!-- Email -->
+		  <div class="row">
+		    <div class="col-xs-12">
+			  <button class="btn btn-primary form-control"><b>Get Quotation Price</b></button>
+			</div>
+		  </div>
+		  <!-- Email -->
+		  
+		</div>
+      </div>
+    </div>
+
+  </div>
+</div>
+<!-- Modal ::: End -->
 <div class="container-fluid" style="height:52px;">
  <nav class="navbar navbar-custom navbar-fixed-top" style="background-color:#000;">
   <div class="container-fluid">
@@ -69,14 +166,21 @@ body::-webkit-scrollbar-thumb { background-color: #000000; }
 	  <div class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
 	  <i class="fa fa-bars" style="color:#f77d0e;"></i>
 	  </div>
-      <a class="navbar-brand" href="#"><span class="a-white navbar-brand-span">Assignlancer</span></a>
+      <a class="navbar-brand" href="#">
+	    <span class="a-white navbar-brand-span">
+	      <span style="color:#f77d0e;">h</span>ere 
+	      <span style="color:#f77d0e;">w</span>e 
+	      <span style="color:#f77d0e;">g</span>et
+	    </span>
+	  </a>
     </div>
 	<div class="collapse navbar-collapse" id="myNavbar">
 	  <ul id="al-menu" class="nav navbar-nav">
-		<li id="appInitHeader-home" class="active"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>"><b>Home</b></a></li>
+		<li id="appInitHeader-home"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>"><b>Home</b></a></li>
 		<li id="appInitHeader-aboutUs"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>app/aboutUs"><span><b>About Us</b></span></a></li>
 		<li id="appInitHeader-services"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>app/services"><span><b>Services</b></span></a></li>
 		<li id="appInitHeader-howItWorks"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>app/howItWorks"><span><b>How it Works?</b></span></a></li>
+		<li id="appInitHeader-pricequote" data-toggle="modal" data-target="#priceQuotationModal"><a href="#" onclick="javascript:selectAppInitHeader('appInitHeader-pricequote');"><span><b>Price Quotation</b></span></a></li>
 		<li id="appInitHeader-auth"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>app/authentication"><span><b>Login/Register</b></span></a></li>
 		<li id="appInitHeader-contactUs"><a href="<?php echo $_SESSION["PROJECT_URL"]; ?>app/contactUs"><span><b>Contact Us</b></span></a></li>
     </ul>
